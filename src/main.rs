9fce7cc32d2561bdd1bb7698b79a883df522443f
arunura <arun.iit@gmail.com>
2022-01-13 07:52:33 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+use std::fs;
+
+fn main() {
+    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+
+        println!("Connection established!");
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let contents = fs::read_to_string("htdocs/index.html").unwrap();
+
+    /*
+    println!(
+        "Request: {}",
+        String::from_utf8_lossy(&buffer[..])
+    );
+    */
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+
+}
